e39bc89059a82f5c9bcccf6e315bd5a9e0bd4b77
Lenocam <reed.macconnell@gmail.com>
2013-12-06 13:44:00 -0500

    second commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Agiler4::Application.config.secret_key_base = 'a21b11b017e2e2923a3698e05b6a025e780e74996c1c3b8ebb4d02160d9e55e482f89a651121e911116cdfcaf9f7e40566e57a5367011321153098233e94266e'
